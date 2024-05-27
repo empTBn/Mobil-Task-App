dec12c0db5aa24d9a52b11819d524b12c80fd3a2
MarielaSolanoG <128342193+MarielaSolanoG@users.noreply.github.com>
2024-05-27 16:32:00 -0600

    Se añade interfaz de register

--- Prueba/app/src/main/java/com/example/prueba/MainActivity.kt
+++ Prueba/app/src/main/java/com/example/prueba/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.prueba
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+        val registerButton = findViewById<Button>(R.id.register_btn)
+        registerButton.setOnClickListener {
+            // Crear un Intent para abrir la nueva actividad
+            val intent = Intent(this, RegisterActivity::class.java)
+            // Iniciar la nueva actividad
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
