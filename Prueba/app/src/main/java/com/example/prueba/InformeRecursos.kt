7c0108d7dc3f506df2165d572e13a5012a7bb80c
MarielaSolanoG <128342193+MarielaSolanoG@users.noreply.github.com>
2024-05-27 20:55:10 -0600

    se agrega pantalla informe de recursos

---
+++ Prueba/app/src/main/java/com/example/prueba/InformeRecursos.kt
@@ -0,0 +1,22 @@
+package com.example.prueba
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class InformeRecursos: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_informe_recursos)
+        val returnButton = findViewById<Button>(R.id.return_btn)
+        returnButton.setOnClickListener {
+            // Crear un Intent para abrir la nueva actividad
+            val intent = Intent(this, MainPageActivity::class.java)
+            // Iniciar la nueva actividad
+            startActivity(intent)
+        }
+    }
+}
